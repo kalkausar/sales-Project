35157d69a1ab78b4a996f9971ccd491b7a27c4bd
kalkausar <kalkausar@student.gunadarma.ac.id>
2019-05-20 14:59:27 +0700

    fix view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
       </div>
     </div>
   </div>
-@endforeach
+  @endforeach
   <!-- <div class="site-blocks-cover overlay" style="background-image: url(images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
     <div class="container">
       <div class="row align-items-center justify-content-center text-center">
@@ -241,25 +241,6 @@
       </div>
     </div>
   </div>
-
-</div>
-
-<!-- <div class="site-section bg-light">
-
-    </div> -->
-
-
-<div class="site-blocks-cover overlay inner-page-cover" style="background-image: url(images/hero_bg_2.jpg); background-attachment: fixed;">
-  <div class="container">
-    <div class="row align-items-center justify-content-center text-center">
-
-      <div class="col-md-7" data-aos="fade-up" data-aos-delay="400">
-        <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-single-big mb-4 d-inline-block popup-vimeo"><span class="icon-play"></span></a>
-        <h2 class="text-white font-weight-light mb-5 h1">Experience Our Outstanding Services</h2>
-
-      </div>
-    </div>
-  </div>
 </div>
 
 <div class="site-section bg-light">
@@ -334,52 +315,6 @@
         </div>
       </div>
 
-    </div>
-  </div>
-</div>
-
-
-<div class="site-section">
-  <div class="container">
-    <div class="row justify-content-center mb-5">
-      <div class="col-md-7 text-center">
-        <h2 class="font-weight-light text-black">Our Blog</h2>
-        <p class="color-black-opacity-5">See Our Daily News &amp; Updates</p>
-      </div>
-    </div>
-    <div class="row mb-3 align-items-stretch">
-      <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
-        <div class="h-entry">
-          <img src="images/hero_bg_1.jpg" alt="Image" class="img-fluid">
-          <h2 class="font-size-regular"><a href="#">How to Plan Your Vacation</a></h2>
-          <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
-        </div>
-      </div>
-      <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
-        <div class="h-entry">
-          <img src="images/hero_bg_2.jpg" alt="Image" class="img-fluid">
-          <h2 class="font-size-regular"><a href="#">How to Plan Your Vacation</a></h2>
-          <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-12 text-center">
-        <a href="#" class="btn btn-outline-primary border-2 py-3 px-5">View All Blog Posts</a>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="site-section border-top">
-  <div class="container">
-    <div class="row text-center">
-      <div class="col-md-12">
-        <h2 class="mb-5 text-black">Want To Travel With Us?</h2>
-        <p class="mb-0"><a href="booking.html" class="btn btn-primary py-3 px-5 text-white">Book Now</a></p>
-      </div>
     </div>
   </div>
 </div>

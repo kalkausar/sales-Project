0eec42c0cd93c13d42c64419da69ea4252ce70e4
kalkausar <kalkausar@student.gunadarma.ac.id>
2019-05-29 13:44:16 +0700

    add favicon

--- resources/views/frontend/template.blade.php
+++ resources/views/frontend/template.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+  <link rel="icon" href="{{url('images/logo-2.png')}}">
   <title>Mitsubishi</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
